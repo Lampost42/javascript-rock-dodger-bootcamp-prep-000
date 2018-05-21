a7d2d0db5443201f42132275446623838a0a31d2
Trevor Russell <truss.42@gmail.com>
2018-05-21 03:30:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ function createRock(x) {
    * seems like a good pace.)
    */
   function moveRock(rock) {
-    console.log("move");
     function step(){
       var rockTop = positionToInteger(rock.style.top);
       if (checkCollision(rock) === true){
